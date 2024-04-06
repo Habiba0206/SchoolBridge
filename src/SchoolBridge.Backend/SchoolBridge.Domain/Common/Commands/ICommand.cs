89bca3bb4d886f7f14e6e4628c71086334d5556f
Habiba0206 <habibasattorova26@gmail.com>
2024-04-06 14:38:40 +0500

    Fixed docs in ICommand

--- src/SchoolBridge.Backend/SchoolBridge.Domain/Common/Commands/ICommand.cs
+++ src/SchoolBridge.Backend/SchoolBridge.Domain/Common/Commands/ICommand.cs
@@ -5,14 +5,14 @@ namespace SchoolBridge.Domain.Common.Commands;
 /// <summary>
 /// Represents a command in a CQRS (Command Query Responsibility Segregation) architecture.
 /// </summary>
-/// <typeparam name="TResult"> the type of result produced by executing the command. </typeparam>
+/// <typeparam name="TResult">The type of result produced by executing the command. </typeparam>
 public interface ICommand<out TResult> : ICommand, IRequest<TResult> 
 {
 
 }
 
 /// <summary>
-/// Defines interface for commands
+/// Defines interface for commands in a CQRS architecture.
 /// </summary>
 public interface ICommand
 {
