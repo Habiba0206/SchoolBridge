b010f4c4d761bdcd703d94504e358d6b8d7e6263
Habiba0206 <habibasattorova26@gmail.com>
2024-04-06 21:33:13 +0500

    Created School entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Domain/Entities/School.cs
@@ -0,0 +1,29 @@
+﻿using SchoolBridge.Domain.Common.Entities;
+
+namespace SchoolBridge.Domain.Entities;
+
+/// <summary>
+/// Represents school entity in the system.
+/// </summary>
+public class School : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the name of the school.
+    /// </summary>
+    public string Name { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the descripton about the school.
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the rating of the school.
+    /// </summary>
+    public float Rate { get; set; }
+
+    /// <summary>
+    /// Gets or sets the collection of the users related to this school.
+    /// </summary>
+    public IEnumerable<User> Users { get; set; }
+}
