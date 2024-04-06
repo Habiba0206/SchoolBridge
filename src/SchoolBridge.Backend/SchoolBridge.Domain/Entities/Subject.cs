23532f12bf5c4e3df681f85413ee2201d60d303f
Habiba0206 <habibasattorova26@gmail.com>
2024-04-06 21:33:28 +0500

    Created Subject entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Domain/Entities/Subject.cs
@@ -0,0 +1,14 @@
+﻿using SchoolBridge.Domain.Common.Entities;
+
+namespace SchoolBridge.Domain.Entities;
+
+/// <summary>
+/// Represents the subject entity in the system.
+/// </summary>
+public class Subject : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the name of the subject.
+    /// </summary>
+    public string Name;
+}
---
+++ src/SchoolBridge.Backend/SchoolBridge.Domain/Entities/TeacherSubject.cs
@@ -0,0 +1,30 @@
+﻿using SchoolBridge.Domain.Common.Entities;
+
+namespace SchoolBridge.Domain.Entities;
+
+/// <summary>
+/// Represents a linking table between `User` (if its role is Teacher) and `Subject` entities
+/// This entity is used to establish many-to-many relationship.
+/// </summary>
+public class TeacherSubject : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets unique identifier of the associated Teacher.
+    /// </summary>
+    public Guid TeacherId { get; set; }
+
+    /// <summary>
+    /// Gets or sets unique identifier of the associated Subject.
+    /// </summary>
+    public Guid SubjectId { get; set; }
+
+    /// <summary>
+    /// Gets or sets associated Teacher with this entity.
+    /// </summary>
+    public User Teacher { get; set; }
+
+    /// <summary>
+    /// Gets or sets associated Subject with this entity.
+    /// </summary>
+    public Subject Subject { get; set; }
+}
