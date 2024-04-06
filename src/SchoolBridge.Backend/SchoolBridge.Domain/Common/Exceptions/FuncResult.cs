bf57be7259a1804bca0559cbefa9542300f212b9
Habiba0206 <habibasattorova26@gmail.com>
2024-04-06 14:39:17 +0500

    Created FuncResult class

---
+++ src/SchoolBridge.Backend/SchoolBridge.Domain/Common/Exceptions/FuncResult.cs
@@ -0,0 +1,37 @@
+﻿namespace SchoolBridge.Domain.Common.Exceptions;
+
+/// <summary>
+/// Represents the result of a function that can return either data of type T or an exception.
+/// </summary>
+/// <typeparam name="T">The type of data returned on success.</typeparam>
+public record FuncResult<T>
+{
+    /// <summary>
+    /// Gets the data result when the operation is successful.
+    /// </summary>
+    public T Data { get; init; }
+
+    /// <summary>
+    /// Gets the exception if the operation encounters the error, otherwise, is null.
+    /// </summary>
+    public Exception? Exception { get; }
+
+    /// <summary>
+    /// Gets the value indicating whether the operation was successful.
+    /// </summary>
+    public bool IsSuccess => Exception is null;
+
+    /// <summary>
+    /// Initializes a new instance of the `FuncResult`.
+    /// Class with successful data.
+    /// </summary>
+    /// <param name="data">The data result of type T.</param>
+    public FuncResult(T data) => Data = data;
+
+    /// <summary>
+    /// Initializes a new instance of the `FuncResult`.
+    /// Class with an exception.
+    /// </summary>
+    /// <param name="exception">Th exception encountered during the operation.</param>
+    public FuncResult(Exception exception) => Exception = exception;
+}
