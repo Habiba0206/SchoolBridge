d5a00717053522e844611a161266ff382770064b
Habiba0206 <habibasattorova26@gmail.com>
2024-04-06 21:32:45 +0500

    Created Class entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Domain/Entities/Class.cs
@@ -0,0 +1,34 @@
+﻿using SchoolBridge.Domain.Common.Entities;
+
+namespace SchoolBridge.Domain.Entities;
+
+/// <summary>
+/// Represents the class entity in the system.
+/// </summary>
+public class Class : AuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the name of the class.
+    /// </summary>
+    public string Name { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the degree of the class.
+    /// </summary>
+    public int Degree { get; set; }
+
+    /// <summary>
+    /// Gets or sets the rating of the class.
+    /// </summary>
+    public float Rate { get; set; }
+
+    /// <summary>
+    /// Gets or sets the unique identifier of the class's head teacher.
+    /// </summary>
+    public Guid HeadTeacherId { get; set; }
+
+    /// <summary>
+    /// Navigation property that stores Teacher user related to this Class.
+    /// </summary>
+    public User Teacher { get; set; }
+}
