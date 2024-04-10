74a32c7e593bb613dfdf8abf30d1a7611e890734
Habiba0206 <habibasattorova26@gmail.com>
2024-04-10 22:19:51 +0500

    Created entity configuration for subject entities

---
+++ src/SchoolBridge.Backend/SchoolBridge.Persistence/EntityConfiguration/SubjectConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolBridge.Domain.Entities;
+
+namespace SchoolBridge.Persistence.EntityConfiguration;
+
+public class SubjectConfiguration : IEntityTypeConfiguration<Subject>
+{
+    public void Configure(EntityTypeBuilder<Subject> builder)
+    {
+        builder
+            .Property(subject => subject.Name)
+            .HasMaxLength(100)
+            .IsRequired(true);
+    }
+}
