f28c0127ca81908a22fcc883c1c309c6d0d799fb
Habiba0206 <habibasattorova26@gmail.com>
2024-04-10 22:18:29 +0500

    Created data base context

---
+++ src/SchoolBridge.Backend/SchoolBridge.Persistence/DataContexts/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolBridge.Domain.Entities;
+
+namespace SchoolBridge.Persistence.DataContexts;
+
+public class AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : DbContext(dbContextOptions)
+{
+    #region School things
+    
+    public DbSet<School> Schools => Set<School>();
+
+    public DbSet<Class> Classes => Set<Class>();
+
+    public DbSet<Subject> Subjects => Set<Subject>();  
+
+    public DbSet<TeacherSubject> TeacherSubjects => Set<TeacherSubject>();
+
+    #endregion
+
+    #region Identity
+
+    public DbSet<User> Users => Set<User>();
+
+    #endregion
+
+    #region Comments 
+
+    public DbSet<Comment> Comments => Set<Comment>();
+
+    #endregion
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
