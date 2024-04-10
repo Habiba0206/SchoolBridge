6dcb7b642298bc6f5840d50c19ea92c870b2643e
Habiba0206 <habibasattorova26@gmail.com>
2024-04-10 22:19:58 +0500

    Created entity configuration for teacher entities

---
+++ src/SchoolBridge.Backend/SchoolBridge.Persistence/EntityConfiguration/TeacherConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolBridge.Domain.Entities;
+
+namespace SchoolBridge.Persistence.EntityConfiguration;
+
+public class TeacherConfiguration : IEntityTypeConfiguration<Teacher>
+{
+    public void Configure(EntityTypeBuilder<Teacher> builder)
+    {
+        builder
+            .HasOne(teacher => teacher.Class)
+            .WithOne(@class => @class.HeadTeacher)
+            .HasForeignKey<Teacher>(teacher => teacher.ClassId);
+
+        builder
+            .HasMany(teacher => teacher.Subjects)
+            .WithMany(subject => subject.Teachers)
+            .UsingEntity<TeacherSubject>(teacherSubject =>
+            {
+                teacherSubject.HasKey(relation =>
+                new
+                {
+                    relation.TeacherId,
+                    relation.SubjectId
+                });
+
+                teacherSubject.ToTable("TeacherSubjects");
+            });
+    }
+}
