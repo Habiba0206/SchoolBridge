9789166f58df56ca749ac8a72c6d37a038803476
Habiba0206 <habibasattorova26@gmail.com>
2024-04-10 22:19:09 +0500

    Created entity configuration for comment entities

---
+++ src/SchoolBridge.Backend/SchoolBridge.Persistence/EntityConfiguration/CommentConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolBridge.Domain.Entities;
+
+namespace SchoolBridge.Persistence.EntityConfiguration;
+
+public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+{
+    public void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder
+            .Property(comment => comment)
+            .IsRequired(true);
+    }
+}
