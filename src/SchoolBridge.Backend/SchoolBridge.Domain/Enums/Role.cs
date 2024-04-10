b61f96ac9df2bfbca0a5e1d83a408c83bffdd705
Habiba0206 <habibasattorova26@gmail.com>
2024-04-10 22:18:13 +0500

    Added docs to the enum `Role`

--- src/SchoolBridge.Backend/SchoolBridge.Domain/Enums/Role.cs
+++ src/SchoolBridge.Backend/SchoolBridge.Domain/Enums/Role.cs
@@ -1,10 +1,17 @@
 ﻿namespace SchoolBridge.Domain.Enums;
 
+/// <summary>
+/// Represents different roles that users can have in the system.
+/// </summary>
 public enum Role
 {
+    /// <summary>
+    /// student role for users
+    /// </summary>
     Student = 0,
 
+    /// <summary>
+    /// teacher roles for users
+    /// </summary>
     Teacher = 1,
-
-    Admin = 2
 }
