047fe592361de93f603238df7f5f3657b99a673a
Habiba0206 <habibasattorova26@gmail.com>
2024-04-10 22:19:19 +0500

    Created entity configuration for student entities

---
+++ src/SchoolBridge.Backend/SchoolBridge.Persistence/EntityConfiguration/SchoolConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolBridge.Domain.Entities;
+
+namespace SchoolBridge.Persistence.EntityConfiguration;
+
+public class SchoolConfiguration : IEntityTypeConfiguration<School>
+{
+    public void Configure(EntityTypeBuilder<School> builder)
+    {
+        builder
+            .Property(school => school.Name)
+            .HasMaxLength(250)
+            .IsRequired();
+
+        builder
+            .HasMany(school => school.Users)
+            .WithOne(user => user.School);
+    }
+}
