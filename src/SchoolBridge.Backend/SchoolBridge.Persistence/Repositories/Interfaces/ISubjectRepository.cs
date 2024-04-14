44f260e48c20ebe4b983b50c91e3b92a99f082a8
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-14 22:14:19 +0500

    Created repository for subject entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Persistence/Repositories/Interfaces/ISubjectRepository.cs
@@ -0,0 +1,104 @@
+﻿using SchoolBridge.Domain.Common.Commands;
+using SchoolBridge.Domain.Common.Queries;
+using SchoolBridge.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace SchoolBridge.Persistence.Repositories.Interfaces;
+
+/// <summary>
+/// Represents a repository interface for managing subject entities.
+/// </summary>
+public interface ISubjectRepository
+{
+    /// <summary>
+    /// Retrieves subjects based on optional filtering conditions and tracking prefrences.
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <param name="queryOptions"></param>
+    /// <returns></returns>
+    IQueryable<Subject> Get(
+         Expression<Func<Subject, bool>>? predicate = default,
+         QueryOptions queryOptions = default);
+
+    /// <summary>
+    /// Asynchronously retrieves a subject by its id, optionally applying caching.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="queryOptions"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<Subject?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Checks if subject exists.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously retrieves subjects by collection of ids.
+    /// </summary>
+    /// <param name="ids"></param>
+    /// <param name="queryOptions"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<IList<Subject>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously creates a new subject entity.
+    /// </summary>
+    /// <param name="subject"></param>
+    /// <param name="commandOptions"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<Subject> CreateAsync(
+        Subject subject,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously updates a subject entity.
+    /// </summary>
+    /// <param name="subject"></param>
+    /// <param name="commandOptions"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<Subject> UpdateAsync(
+        Subject subject,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously deletes a subject entity.
+    /// </summary>
+    /// <param name="subject"></param>
+    /// <param name="commandOptions"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<Subject?> DeleteAsync(
+        Subject subject,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously deletes an existing subject entity by its id.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="commandOptions"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<Subject?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default);
+}
---
+++ src/SchoolBridge.Backend/SchoolBridge.Persistence/Repositories/SubjectRepository.cs
@@ -0,0 +1,64 @@
+﻿using SchoolBridge.Domain.Common.Commands;
+using SchoolBridge.Domain.Common.Queries;
+using SchoolBridge.Domain.Entities;
+using SchoolBridge.Persistence.Caching.Brokers;
+using SchoolBridge.Persistence.DataContexts;
+using SchoolBridge.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace SchoolBridge.Persistence.Repositories;
+
+/// <summary>
+/// Provides data access functionality for Subject entities.
+/// Inheriting core repository logic from `EntityRepositoryBase`.
+/// </summary>
+public class SubjectRepository(AppDbContext appDbContext, ICacheBroker cacheBroker) :
+    EntityRepositoryBase<Subject, AppDbContext>(appDbContext, cacheBroker),
+    ISubjectRepository
+{
+    public IQueryable<Subject> Get(
+        Expression<Func<Subject, bool>>? predicate = null,
+        QueryOptions queryOptions = default) =>
+    base.Get(predicate, queryOptions);
+
+    public ValueTask<Subject?> GetByIdAsync(
+        Guid id, 
+        QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<Subject>> GetByIdsAsync(
+        IEnumerable<Guid> ids, 
+        QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+    base.CheckByIdAsync(id, cancellationToken);
+
+    public ValueTask<Subject> CreateAsync(
+        Subject subject, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default) =>
+    base.CreateAsync(subject, commandOptions, cancellationToken);
+
+    public ValueTask<Subject> UpdateAsync(
+        Subject subject, 
+        CommandOptions commandOptions, 
+        CancellationToken cancellationToken = default) =>
+    base.UpdateAsync(subject, commandOptions, cancellationToken);
+
+    public ValueTask<Subject?> DeleteAsync(
+        Subject subject,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default) =>
+    base.DeleteAsync(subject, commandOptions, cancellationToken);
+
+    public ValueTask<Subject?> DeleteByIdAsync(
+        Guid id, 
+        CommandOptions commandOptions, 
+        CancellationToken cancellationToken = default) =>
+    base.DeleteByIdAsync(id, commandOptions, cancellationToken);
+}
