81c63d70bb04f3418b388590ed5e45d2ba884f51
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-15 04:17:31 +0500

    Created queries for class entities

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Classes/Queries/ClassGetByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using SchoolBridge.Application.Classes.Models;
+using SchoolBridge.Domain.Common.Queries;
+
+namespace SchoolBridge.Application.Classes.Queries;
+
+/// <summary>
+/// Represents a query to retrieve a specific class by its ID.
+/// </summary>
+public record ClassGetByIdQuery : IQuery<ClassDto?>
+{
+    /// <summary>
+    /// The uniqueidentifier of the class to retrieve.
+    /// </summary>
+    public Guid ClassId { get; set; }
+}
---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Classes/Queries/ClassGetQuery.cs
@@ -0,0 +1,15 @@
+﻿using SchoolBridge.Application.Classes.Models;
+using SchoolBridge.Domain.Common.Queries;
+
+namespace SchoolBridge.Application.Classes.Queries;
+
+/// <summary>
+/// Represents a query to retrieve the collection of class by its filter.
+/// </summary>
+public record ClassGetQuery : IQuery<ICollection<ClassDto>>
+{
+    /// <summary>
+    /// The filter pagination to retrieve the collection of class entities.
+    /// </summary>
+    public ClassFilter ClassFilter { get; set; }
+}
