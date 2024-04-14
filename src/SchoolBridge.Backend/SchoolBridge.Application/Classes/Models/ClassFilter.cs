b5c3de96233ba37cb6d4f4616d858c5f376d2f99
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-15 02:33:39 +0500

    Created filter model for class entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Classes/Models/ClassFilter.cs
@@ -0,0 +1,29 @@
+﻿using SchoolBridge.Domain.Common.Queries;
+
+namespace SchoolBridge.Application.Classes.Models;
+
+public class ClassFilter : FilterPagination
+{
+    /// <summary>
+    /// Overrides base GetHashCode method
+    /// </summary>
+    /// <returns></returns>
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+
+        return hashCode.ToHashCode();
+    }
+
+    /// <summary>
+    /// Overrides base Equels method
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public override bool Equals(object? obj) =>
+        obj is ClassFilter classFilter 
+        && classFilter.GetHashCode() == GetHashCode();
+}
