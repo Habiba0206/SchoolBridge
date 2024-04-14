cf164bb2939f169dbf9ed837f72c123a8285563e
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-14 22:14:09 +0500

    Created repository for student entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Persistence/Repositories/Interfaces/IStudentRepository.cs
@@ -0,0 +1,104 @@
+﻿using SchoolBridge.Domain.Common.Commands;
+using SchoolBridge.Domain.Common.Queries;
+using SchoolBridge.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace SchoolBridge.Persistence.Repositories.Interfaces;
+
+/// <summary>
+/// Represents a repository interface for managing student entities.
+/// </summary>
+public interface IStudentRepository
+{
+    /// <summary>
+    /// Retrieves students based on optional filtering conditions and tracking prefrences.
+    /// </summary>
+    /// <param name="predicate"></param>
+    /// <param name="queryOptions"></param>
+    /// <returns></returns>
+    IQueryable<Student> Get(
+         Expression<Func<Student, bool>>? predicate = default,
+         QueryOptions queryOptions = default);
+
+    /// <summary>
+    /// Asynchronously retrieves a student by its id, optionally applying caching.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="queryOptions"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<Student?> GetByIdAsync(
+        Guid id,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Checks if student exists.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously retrieves students by collection of ids.
+    /// </summary>
+    /// <param name="ids"></param>
+    /// <param name="queryOptions"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<IList<Student>> GetByIdsAsync(
+        IEnumerable<Guid> ids,
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously creates a new student entity.
+    /// </summary>
+    /// <param name="student"></param>
+    /// <param name="commandOptions"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<Student> CreateAsync(
+        Student student,
+        CommandOptions commandOptions = default,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously updates a student entity.
+    /// </summary>
+    /// <param name="student"></param>
+    /// <param name="commandOptions"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<Student> UpdateAsync(
+        Student student,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously deletes a student entity.
+    /// </summary>
+    /// <param name="student"></param>
+    /// <param name="commandOptions"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<Student?> DeleteAsync(
+        Student student,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Asynchronously deletes an existing student entity by its id.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="commandOptions"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    ValueTask<Student?> DeleteByIdAsync(
+        Guid id,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default);
+}
---
+++ src/SchoolBridge.Backend/SchoolBridge.Persistence/Repositories/StudentRepository.cs
@@ -0,0 +1,64 @@
+﻿using SchoolBridge.Domain.Common.Commands;
+using SchoolBridge.Domain.Common.Queries;
+using SchoolBridge.Domain.Entities;
+using SchoolBridge.Persistence.Caching.Brokers;
+using SchoolBridge.Persistence.DataContexts;
+using SchoolBridge.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace SchoolBridge.Persistence.Repositories;
+
+/// <summary>
+/// Provides data access functionality for Student entities.
+/// Inheriting core repository logic from `EntityRepositoryBase`.
+/// </summary>
+public class StudentRepository(AppDbContext appDbContext, ICacheBroker cacheBroker) :
+    EntityRepositoryBase<Student, AppDbContext>(appDbContext, cacheBroker),
+    IStudentRepository
+{
+    public IQueryable<Student> Get(
+        Expression<Func<Student, bool>>? predicate = null, 
+        QueryOptions queryOptions = default) =>
+    base.Get(predicate, queryOptions);
+
+    public ValueTask<Student?> GetByIdAsync(
+        Guid id, 
+        QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<Student>> GetByIdsAsync(
+        IEnumerable<Guid> ids, 
+        QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default) =>
+    base.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default) =>
+    base.CheckByIdAsync(id, cancellationToken);
+
+    public ValueTask<Student> CreateAsync(
+        Student student, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default) =>
+    base.CreateAsync(student, commandOptions, cancellationToken);
+
+    public ValueTask<Student> UpdateAsync(
+        Student student, 
+        CommandOptions commandOptions, 
+        CancellationToken cancellationToken = default) =>
+    base.UpdateAsync(student, commandOptions, cancellationToken);
+
+    public ValueTask<Student?> DeleteAsync(
+        Student student,
+        CommandOptions commandOptions,
+        CancellationToken cancellationToken = default) =>
+    base.UpdateAsync(student, commandOptions, cancellationToken);
+
+    public ValueTask<Student?> DeleteByIdAsync(
+        Guid id, 
+        CommandOptions commandOptions, 
+        CancellationToken cancellationToken = default) =>
+    base.DeleteByIdAsync(id, commandOptions, cancellationToken);
+}
