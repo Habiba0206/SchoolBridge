d16ba640c511298844fec4dce1ee7243b71224cf
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-15 02:32:55 +0500

    Created command for updating class entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Classes/Commands/ClassUpdateCommand.cs
@@ -0,0 +1,15 @@
+﻿using SchoolBridge.Application.Classes.Models;
+using SchoolBridge.Domain.Common.Commands;
+
+namespace SchoolBridge.Application.Classes.Commands;
+
+/// <summary>
+/// Represents a command to modify an existing class.
+/// </summary>
+public record ClassUpdateCommand : ICommand<ClassDto>
+{
+    /// <summary>
+    /// The data required to update an existing class.
+    /// </summary>
+    public ClassDto ClassDto { get; set; }
+}
