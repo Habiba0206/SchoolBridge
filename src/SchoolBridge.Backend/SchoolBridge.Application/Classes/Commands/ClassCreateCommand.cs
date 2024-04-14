de4d04f4cf0d91b19fbe5aaca8ab1cbf158abb7c
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-15 02:32:44 +0500

    Created command for creating class entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Classes/Commands/ClassCreateCommand.cs
@@ -0,0 +1,15 @@
+﻿using SchoolBridge.Application.Classes.Models;
+using SchoolBridge.Domain.Common.Commands;
+
+namespace SchoolBridge.Application.Classes.Commands;
+
+/// <summary>
+/// Represents a command to create a new class.
+/// </summary>
+public record ClassCreateCommand : ICommand<ClassDto>
+{
+    /// <summary>
+    /// The data required to create a new class.
+    /// </summary>
+    public ClassDto ClassDto { get; set; }
+}
