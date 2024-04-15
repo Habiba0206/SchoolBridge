ff80a92f42d53c2270d9b6027dfc8821cbba4865
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-15 07:20:45 +0500

    Created filter model for comment entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Comments/Models/CommentFilter.cs
@@ -0,0 +1,30 @@
+﻿using SchoolBridge.Application.Classes.Models;
+using SchoolBridge.Domain.Common.Queries;
+
+namespace SchoolBridge.Application.Comments.Models;
+
+public class CommentFilter : FilterPagination
+{
+    /// <summary>
+    /// Overrides base GetHashCode method
+    /// </summary>
+    /// <returns></returns>
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+
+        return hashCode.ToHashCode();
+    }
+
+    /// <summary>
+    /// Overrides base Equels method
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public override bool Equals(object? obj) =>
+        obj is CommentFilter commentFilter
+        && commentFilter.GetHashCode() == GetHashCode();
+}
