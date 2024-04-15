a2b46af532b417d822c9630430f94c31af16cd01
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-15 07:21:14 +0500

    Created command for creating comment entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Comments/Commands/CommentCreateCommend.cs
@@ -0,0 +1,15 @@
+﻿using SchoolBridge.Application.Comments.Models;
+using SchoolBridge.Domain.Common.Commands;
+
+namespace SchoolBridge.Application.Comments.Commands;
+
+/// <summary>
+/// Represents a command to create a new comment.
+/// </summary>
+public record CommentCreateCommend : ICommand<CommentDto>
+{
+    /// <summary>
+    /// The data required to create a new comment.
+    /// </summary>
+    public CommentDto CommentDto { get; set; }
+}
