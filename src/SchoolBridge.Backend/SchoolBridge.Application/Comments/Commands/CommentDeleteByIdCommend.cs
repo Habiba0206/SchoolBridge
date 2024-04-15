eb23fb004d86ef89a5311524e33f1538a1c0fd9d
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-15 07:21:33 +0500

    Created command for deleting comment entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Comments/Commands/CommentDeleteByIdCommend.cs
@@ -0,0 +1,14 @@
+﻿using SchoolBridge.Domain.Common.Commands;
+
+namespace SchoolBridge.Application.Comments.Commands;
+
+/// <summary>
+/// Represents a command to delete an existing comment.
+/// </summary>
+public record CommentDeleteByIdCommend : ICommand<bool>
+{
+    /// <summary>
+    /// The unique identifier of the comment to be deleted
+    /// </summary>
+    public Guid CommentId { get; set; }
+}
