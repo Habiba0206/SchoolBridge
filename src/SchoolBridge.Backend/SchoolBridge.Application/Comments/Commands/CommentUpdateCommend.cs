020a629483ed1837ce5edd94bf878730c0b47e4b
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-15 07:21:23 +0500

    Created command for updating comment entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Comments/Commands/CommentUpdateCommend.cs
@@ -0,0 +1,15 @@
+﻿using SchoolBridge.Application.Comments.Models;
+using SchoolBridge.Domain.Common.Commands;
+
+namespace SchoolBridge.Application.Comments.Commands;
+
+/// <summary>
+/// Represents a command to modify an existing comment.
+/// </summary>
+public record CommentUpdateCommend : ICommand<CommentDto>
+{
+    /// <summary>
+    /// The data required to update an existing comment.
+    /// </summary>
+    public CommentDto CommandDto { get; set; }
+}
