0cec1915649c3b605d61a38d7b904a5fdacd031d
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-15 07:20:25 +0500

    Created DTO model for comment entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Comments/Models/CommentDto.cs
@@ -0,0 +1,42 @@
+﻿namespace SchoolBridge.Application.Comments.Models;
+
+/// <summary>
+/// Represents a data transfer object (DTO) for a comment.
+/// </summary>
+public class CommentDto
+{
+    /// <summary>
+    /// Gets or sets the unique identifier of the comment. 
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the review of the comment.
+    /// </summary>
+    public string Review { get; set; }
+
+    /// <summary>
+    /// Gets or sets the user of the comment.
+    /// </summary>
+    public Guid FromUserId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the class which this comment is for.
+    /// </summary>
+    public Guid ToClassId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the teacher which this comment is for.
+    /// </summary>
+    public Guid ToTeacherId { get; set; }
+
+    /// <summary>
+    /// Gets or sets some feedback about the school.
+    /// </summary>
+    public string? AboutSchool { get; set; }
+
+    /// <summary>
+    /// Gets or stes the value which determines if the user's comment is anonymous or not.
+    /// </summary>
+    public bool IsAnonymous { get; set; }
+}
