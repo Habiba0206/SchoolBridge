d2e78f01d6e71811f4d71976e641f5e816143a63
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-16 12:28:01 +0500

    Created queries for comment entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Comments/Queries/CommentGetByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using SchoolBridge.Application.Comments.Models;
+using SchoolBridge.Domain.Common.Queries;
+
+namespace SchoolBridge.Application.Comments.Queries;
+
+/// <summary>
+/// Represents a query to retrieve a specific comment by its ID.
+/// </summary>
+public record CommentGetByIdQuery : IQuery<CommentDto?>
+{
+    /// <summary>
+    /// The uniqueidentifier of the comment to retrieve.
+    /// </summary>
+    public Guid CommetId { get; set; }
+}
---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Comments/Queries/CommentGetQuery.cs
@@ -0,0 +1,15 @@
+﻿using SchoolBridge.Application.Comments.Models;
+using SchoolBridge.Domain.Common.Queries;
+
+namespace SchoolBridge.Application.Comments.Queries;
+
+/// <summary>
+/// Represents a query to retrieve the collection of comment by its filter.
+/// </summary>
+public record CommentGetQuery : IQuery<ICollection<CommentDto>>
+{
+    /// <summary>
+    /// The filter pagination to retrieve the collection of comment entities.
+    /// </summary>
+    public CommentFilter CommentFilter { get; set; }
+}
