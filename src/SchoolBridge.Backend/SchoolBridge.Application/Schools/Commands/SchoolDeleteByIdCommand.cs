26773d67d5fb85a2c01a287a78b055810b7cb286
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-18 00:55:49 +0500

    Created command for deleting school entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Schools/Commands/SchoolDeleteByIdCommand.cs
@@ -0,0 +1,14 @@
+﻿using SchoolBridge.Domain.Common.Commands;
+
+namespace SchoolBridge.Application.Schools.Commands;
+
+/// <summary>
+/// Represents a command to delete an existing school.
+/// </summary>
+public class SchoolDeleteByIdCommand : ICommand<bool>
+{
+    /// <summary>
+    /// The unique identifier of the school to be deleted
+    /// </summary>
+    public Guid SchoolId { get; set; }
+}
