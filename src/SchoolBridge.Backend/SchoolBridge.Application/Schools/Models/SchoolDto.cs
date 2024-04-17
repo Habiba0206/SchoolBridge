851817e5b5af90872bc991b4d4460847b1430b3b
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-18 00:54:32 +0500

    Created DTO model for school entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Schools/Models/SchoolDto.cs
@@ -0,0 +1,22 @@
+﻿namespace SchoolBridge.Application.Schools.Models;
+
+/// <summary>
+/// Represents a data transfer object (DTO) for a school.
+/// </summary>
+public class SchoolDto
+{
+    /// <summary>
+    /// Gets or sets the name of the school.
+    /// </summary>
+    public string Name { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the descripton about the school.
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the rating of the school.
+    /// </summary>
+    public float Rate { get; set; }
+}
