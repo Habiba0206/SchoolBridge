5693f603065ba98b1d28b8a52572b9e41f9ff7e6
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-18 01:03:58 +0500

    Created queries for school entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Schools/Queries/SchoolGetByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using SchoolBridge.Application.Schools.Models;
+using SchoolBridge.Domain.Common.Queries;
+
+namespace SchoolBridge.Application.Schools.Queries;
+
+/// <summary>
+/// Represents a query to retrieve a specific school by its ID.
+/// </summary>
+public class SchoolGetByIdQuery : IQuery<SchoolDto?>
+{
+    /// <summary>
+    /// The uniqueidentifier of the school to retrieve.
+    /// </summary>
+    public Guid SchoolId { get; set; }
+}
---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Schools/Queries/SchoolGetQuery.cs
@@ -0,0 +1,15 @@
+﻿using SchoolBridge.Application.Schools.Models;
+using SchoolBridge.Domain.Common.Queries;
+
+namespace SchoolBridge.Application.Schools.Queries;
+
+/// <summary>
+/// Represents a query to retrieve the collection of school by its filter.
+/// </summary>
+public class SchoolGetQuery : IQuery<ICollection<SchoolDto>>
+{
+    /// <summary>
+    /// The filter pagination to retrieve the collection of school entities.
+    /// </summary>
+    public SchoolFilter SchoolFilter { get; set; }
+}
