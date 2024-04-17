55fd6f7382f21652f0c8cfced7b8d4fa16dc1616
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-18 01:17:25 +0500

    Creates queries for student entities

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Students/Queries/StudentGetByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using SchoolBridge.Application.Schools.Models;
+using SchoolBridge.Domain.Common.Queries;
+
+namespace SchoolBridge.Application.Schools.Queries;
+
+/// <summary>
+/// Represents a query to retrieve a specific student by its ID.
+/// </summary>
+public class StudentGetByIdQuery : IQuery<StudentDto?>
+{
+    /// <summary>
+    /// The uniqueidentifier of the student to retrieve.
+    /// </summary>
+    public Guid StudentId { get; set; }
+}
---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Students/Queries/StudentGetQuery.cs
@@ -0,0 +1,15 @@
+﻿using SchoolBridge.Application.Schools.Models;
+using SchoolBridge.Domain.Common.Queries;
+
+namespace SchoolBridge.Application.Schools.Queries;
+
+/// <summary>
+/// Represents a query to retrieve the collection of student by its filter.
+/// </summary>
+public class StudentGetQuery : IQuery<ICollection<StudentDto>>
+{
+    /// <summary>
+    /// The filter pagination to retrieve the collection of student entities.
+    /// </summary>
+    public StudentFilter StudentFilter { get; set; }
+}
