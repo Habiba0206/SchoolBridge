69a42c850038f3a4f4967ff16b19f24a3e1c4ec0
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-18 00:55:04 +0500

    Created command for creating comment entity

---
+++ src/SchoolBridge.Backend/SchoolBridge.Application/Schools/Commands/SchoolCreateCommand.cs
@@ -0,0 +1,15 @@
+﻿using SchoolBridge.Application.Schools.Models;
+using SchoolBridge.Domain.Common.Commands;
+
+namespace SchoolBridge.Application.Schools.Commands;
+
+/// <summary>
+/// Represents a command to create a new school.
+/// </summary>
+public class SchoolCreateCommand : ICommand<SchoolDto>
+{
+    /// <summary>
+    /// The data required to create a new school.
+    /// </summary>
+    public SchoolDto SchoolDto { get; set; }
+}
